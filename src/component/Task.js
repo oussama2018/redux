08c475107c9961c9ae1d054cff2e9b1455f8961c
Oussama <oussamakmar1@gmail.com>
2023-06-26 17:42:05 +0100

    second commit

--- src/component/Task.js
+++ src/component/Task.js
@@ -2,11 +2,16 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { editToDo,editCheck } from '../redux/actions/toDoActions'
 import { useRef } from 'react'
+import { useState } from 'react'
 const Task = ({toDo}) => {
     const dispatch = useDispatch()
     const x=useRef()
     const y=useRef()
-
+    const [filtre,setfiltre]=useState()
+    const handleSearch=()=>{
+      x(x.current.value)
+      y(y.current.value)
+    }
   return (
     <div>
         <h2>{toDo.description}</h2>
@@ -15,7 +20,9 @@ const Task = ({toDo}) => {
         <input type="text" placeholder='edit' ref={x}/>
         <input type="submit" onClick={() => {console.log(x);dispatch(editToDo({id:toDo.id,description:x.current.value}))}}/>
         <input type='checkbox' ref={y} onClick={() => {console.log(x);dispatch(editCheck({isDone:y.current.checked}))}}/>
-        
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
     
 
         </div>
